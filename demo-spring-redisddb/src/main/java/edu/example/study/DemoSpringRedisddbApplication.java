de45c1da7c0576e306659b7034700a08d664bda7
taxuezheng <taxuezheng@bonc.com.cn>
2018-03-29 10:54:00 +0800

    springboot+redisdb

---
+++ demo-spring-redisddb/src/main/java/edu/example/study/DemoSpringRedisddbApplication.java
@@ -0,0 +1,12 @@
+package edu.example.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringRedisddbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringRedisddbApplication.class, args);
+	}
+}
