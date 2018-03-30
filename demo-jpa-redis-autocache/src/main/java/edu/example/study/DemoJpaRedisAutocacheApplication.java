1e5caa749f6783fc6eac6b5f6fc07fb4089dee39
taxuezheng <taxuezheng@bonc.com.cn>
2018-03-30 11:23:27 +0800

    springboot jpa redis自动缓存配置

---
+++ demo-jpa-redis-autocache/src/main/java/edu/example/study/DemoJpaRedisAutocacheApplication.java
@@ -0,0 +1,12 @@
+package edu.example.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaRedisAutocacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaRedisAutocacheApplication.class, args);
+	}
+}
